002b8e4f31f35953e63e7a3c77c52c3d04565252
Jucimar Jr <jucimar.jr>
2023-05-13 13:51:56 -0300

    hello_world

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*
+    Nome:   Jucimar Maia da Silva Jr
+    Mat:    7389217389123
+    
+    Meu primeiro programa =)
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("Hello World!\n");
+	return 0;
+}
